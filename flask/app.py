d6d2b336d90485edb96adfd4d7e378fbfd961009
Alec Slaney <alecg.sla@gmail.com>
2020-04-16 13:57:51 -0700

    Added API call to app

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 import pandas as pd
 import json
+import requests
 
-df = pd.read_csv("../data_clean.csv")
-df.to_json("../data.json", orient="index")
+url = "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RecreationOpportunities_01/MapServer/0/query?where=1%3D1&outFields=RECAREANAME,LONGITUDE,LATITUDE,RECAREAURL,OPEN_SEASON_START,OPEN_SEASON_END,FORESTNAME,RECAREAID,MARKERACTIVITY,MARKERACTIVITYGROUP,RECAREADESCRIPTION,RECPORTAL_UNIT_KEY,FORESTORGCODE,OBJECTID,FEEDESCRIPTION,OPERATIONAL_HOURS,RESERVATION_INFO,RESTRICTIONS,ACCESSIBILITY,OPENSTATUS&returnGeometry=false&outSR=4326&f=json"
+response = requests.get(url).json()
 
-with open("../data.json") as f:
-  data = json.load(f)
+# df = pd.read_csv("../data_clean.csv")
+# df.to_json("../data.json", orient="index")
+
+# with open("../data.json") as f:
+#   data = json.load(f)
 
 # Creation of flask app
 app = Flask(__name__)
@@ -20,7 +24,8 @@ def index():
 
 @app.route("/api/data")
 def apiData():
-    return jsonify(data)
+    return json.dumps(response, indent=1)
+    # return jsonify(data)
 
 # Debugger
 if __name__ == "__main__":
