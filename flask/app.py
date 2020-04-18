503146d4bde214fc14f7809c38cc7363a07164f9
Alec Slaney <alecg.sla@gmail.com>
2020-04-17 23:54:27 -0700

    Rewrote API call functionality, now creates custom JSON

--- flask/app.py
+++ flask/app.py
@@ -1,32 +1,75 @@
 from flask import Flask, jsonify
-import pandas as pd
-import json
-import requests
+import json, requests
 
-url = "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RecreationOpportunities_01/MapServer/0/query?where=1%3D1&outFields=RECAREANAME,LONGITUDE,LATITUDE,RECAREAURL,OPEN_SEASON_START,OPEN_SEASON_END,FORESTNAME,RECAREAID,MARKERACTIVITY,MARKERACTIVITYGROUP,RECAREADESCRIPTION,RECPORTAL_UNIT_KEY,FORESTORGCODE,OBJECTID,FEEDESCRIPTION,OPERATIONAL_HOURS,RESERVATION_INFO,RESTRICTIONS,ACCESSIBILITY,OPENSTATUS&returnGeometry=false&outSR=4326&f=json"
-response = requests.get(url).json()
+def api_call():
 
-# df = pd.read_csv("../data_clean.csv")
-# df.to_json("../data.json", orient="index")
+    count_only = "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RecreationOpportunities_01/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json&returnCountOnly=true"
+    url = "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RecreationOpportunities_01/MapServer/0/query?where=1%3D1&outFields=RECAREANAME,LONGITUDE,LATITUDE,RECAREAURL,OPEN_SEASON_START,OPEN_SEASON_END,FORESTNAME,RECAREAID,MARKERACTIVITY,MARKERACTIVITYGROUP,RECAREADESCRIPTION,RECPORTAL_UNIT_KEY,FORESTORGCODE,OBJECTID,FEEDESCRIPTION,OPERATIONAL_HOURS,RESERVATION_INFO,RESTRICTIONS,ACCESSIBILITY,OPENSTATUS&returnGeometry=false&outSR=4326&f=json"
 
-# with open("../data.json") as f:
-#   data = json.load(f)
+    count = requests.get(count_only).json()
+    total_records = count["count"]
+    print(f"Number of records found: {total_records}")
+    
+    record_count = 1000
+    offset = 0
+    x = 0
+    print(f"Getting records now...")
 
-# Creation of flask app
-app = Flask(__name__)
+    while offset < total_records:
+        get_records =  requests.get(f"{url}&resultOffset={offset}&resultRecordCount={record_count}").json()
 
-@app.route("/")
-def index():
-    return (
-        f"Index of Data API<br/>"
-        f"<a href=/api/data>Click here to see the json<br/>"
-    )
+        data = json.dumps(get_records, sort_keys=True, indent=4)
+        file_name = "jsons/api_calls/json" + str(x)
 
-@app.route("/api/data")
-def apiData():
-    return json.dumps(response, indent=1)
-    # return jsonify(data)
+        with open(file_name, "w") as f:
+            f.write(data)  
+        
+        x +=1
+        offset += 1000
+        if offset > total_records:
+            print(f"Finishing...")
+        else:
+            print(f"Fetched {offset} records so far...")
 
-# Debugger
-if __name__ == "__main__":
-    app.run(debug=True)
+def featureExtract():
+    i = 0
+    file_name = "jsons/api_calls/json0"
+    big_json = {}
+    while i < 14:
+        with open(file_name) as f:
+            d = json.load(f)
+            print(f"Accessing {file_name}")
+            features = d["features"]
+            for x in range(len(features)):
+                attribute = features[x]["attributes"]
+                to_add = {attribute["OBJECTID"]: attribute}
+                big_json.update(to_add)
+                x +=1
+        i +=1
+        file_name = "jsons/api_calls/json" + str(i)
+    
+    to_json = json.dumps(big_json, indent=4, sort_keys=True)
+    with open("jsons/features.json", "w") as f:
+        f.write(to_json)
+
+api_call()
+featureExtract()
+print(f"Complete.")
+
+# # Creation of flask app
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return (
+#         f"Index of Data API<br/>"
+#         f"<a href=/api/data>Click here to see the json<br/>"
+#     )
+
+# @app.route("/api/data")
+# def apiData():
+#     return to_json
+
+# # Debugger
+# if __name__ == "__main__":
+#     app.run(debug=True)
