148ae009a6ce497c7ea130082ea1cc2b8168c73e
Ultimate1991 <57298395+Ultimate1991@users.noreply.github.com>
2020-04-18 12:58:47 -0700

    line 53 commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -50,5 +50,4 @@ for (var index = 0; index < attributes.length; index++) {
   createMap(L.layerGroup(forestMarkers));
 
 // Perform an API call to the National Forest API to get information. Call createMarkers when complete
-d3.json ("https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_RecreationOpportunities_01/MapServer/0/query?where=1%3D1&outFields=RECAREANAME,LONGITUDE,LATITUDE,RECAREAURL,OPEN_SEASON_START,OPEN_SEASON_END,FORESTNAME,RECAREAID,MARKERACTIVITY,MARKERACTIVITYGROUP,RECAREADESCRIPTION,RECPORTAL_UNIT_KEY,FORESTORGCODE,OBJECTID,FEEDESCRIPTION,OPERATIONAL_HOURS,RESERVATION_INFO,RESTRICTIONS,ACCESSIBILITY,OPENSTATUS&returnGeometry=false&outSR=4326&f=json", createMarkers);
-}
+d3.json ("features.json", createMarkers)
